2eab335d36e6a15274e98bdb0d3de3055f97782d
Matthew Wiewel <mjvwiewel@gmail.com>
2020-02-07 19:21:42 -0600

    added SetDimensions3D method

--- Point3D.go
+++ Point3D.go
@@ -11,3 +11,8 @@ var ZMin int
 //ZMax is the max z value allowed
 var ZMax int
 
+//SetDimensions3D sets the the dimensions for the x/y/z planes
+func SetDimensions3D(xmin, xmax, ymin, ymax, zmin, zmax int) {
+	ZMin, ZMax = zmin, zmax
+	SetDimensions2D(xmin, xmax, ymin, ymax)
+}
