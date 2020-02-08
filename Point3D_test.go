8c2057c09989d1fadbd699a7b8a1ff377b9bfb72
Matthew Wiewel <mjvwiewel@gmail.com>
2020-02-07 19:21:27 -0600

    added file for test suite for Point3D

---
+++ Point3D_test.go
@@ -0,0 +1,25 @@
+package point
+
+import "testing"
+
+func TestSetDimensions3D(t *testing.T) {
+    SetDimensions3D(1,2,3,4,5,6)
+    if XMin != 1 {
+		t.Errorf("XMin = %d, not 1", XMin)
+	}
+	if XMax != 2 {
+		t.Errorf("XMax = %d, not 2", XMax)
+	}
+	if YMin != 3 {
+		t.Errorf("YMin = %d, not 3", YMin)
+	}
+	if YMax != 4 {
+		t.Errorf("YMax = %d, not 4", YMax)
+	}
+    if ZMin != 5 {
+        t.Errorf("ZMin = %d, not 5", ZMin)
+    }
+    if ZMax != 6 {
+        t.Errorf("ZMax = %d, not 6", ZMax)
+    }
+}
