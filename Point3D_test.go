5fb80e7c886cb0d949c48151d1b18e9a7e89a147
Matthew Wiewel <mjvwiewel@gmail.com>
2020-02-13 09:53:41 -0600

    began adding DistanceFrom tests

--- Point3D_test.go
+++ Point3D_test.go
@@ -1,7 +1,7 @@
 package point
 
-import(
-	//"math"
+import (
+	"math"
 	"testing"
 )
 
@@ -13,7 +13,7 @@ I am not sure exactly when I'll get around to adding them.
 */
 
 func TestToStringPoint3D(t *testing.T) {
-	p := Point3D{Point2D{1,2},3}
+	p := Point3D{Point2D{1, 2}, 3}
 	expected := "(1,2,3)"
 	result := p.ToString()
 	if result != expected {
@@ -21,9 +21,26 @@ func TestToStringPoint3D(t *testing.T) {
 	}
 }
 
+func TestDistanceFromPoint3D_1(t *testing.T) { //test case where same point
+	p1 := Point3D{Point2D{1, 2}, 3}
+	p2 := Point3D{Point2D{1, 2}, 3}
+	expected := 0.
+	result := p1.DistanceFrom(&p2)
+	diff := math.Abs(result - expected)
+	if diff > epsilon {
+		t.Errorf("Distance from %s to %s was %g, not %g as expected",
+			p1.ToString(), p2.ToString(), result, diff)
+	}
+
+	if p1.DistanceFrom(&p2) != p2.DistanceFrom(&p1) {
+		t.Errorf("Distance From not commutative. Received %g and %g",
+			p1.DistanceFrom(&p2), p2.DistanceFrom(&p1))
+	}
+
+}
 func TestSetDimensions3D(t *testing.T) {
-    SetDimensions3D(1,2,3,4,5,6)
-    if XMin != 1 {
+	SetDimensions3D(1, 2, 3, 4, 5, 6)
+	if XMin != 1 {
 		t.Errorf("XMin = %d, not 1", XMin)
 	}
 	if XMax != 2 {
@@ -35,10 +52,10 @@ func TestSetDimensions3D(t *testing.T) {
 	if YMax != 4 {
 		t.Errorf("YMax = %d, not 4", YMax)
 	}
-    if ZMin != 5 {
-        t.Errorf("ZMin = %d, not 5", ZMin)
-    }
-    if ZMax != 6 {
-        t.Errorf("ZMax = %d, not 6", ZMax)
-    }
+	if ZMin != 5 {
+		t.Errorf("ZMin = %d, not 5", ZMin)
+	}
+	if ZMax != 6 {
+		t.Errorf("ZMax = %d, not 6", ZMax)
+	}
 }
